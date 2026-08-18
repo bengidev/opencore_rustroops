e882d8602c3c649f97c69ed00cef59d0f8de35e0
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-08-18 17:00:22 +0700

    feat(shell): add dimension tween helper

--- src/app/shell/mod.rs
+++ src/app/shell/mod.rs
@@ -2,6 +2,7 @@
 
 mod chrome;
 pub mod tabs;
+pub mod tween;
 
 pub use chrome::{
     BOTTOM_DEFAULT, BOTTOM_MAX_VH, BOTTOM_MIN, RIGHT_DEFAULT, RIGHT_MAX, RIGHT_MIN,
@@ -9,3 +10,4 @@ pub use chrome::{
     clamp_bottom_height, clamp_right_width, clamp_sidebar_width,
 };
 pub use tabs::TabModel;
+pub use tween::{DimTween, RESIZE_MS, ease_out, eval_tween, tween_finished};
